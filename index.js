4c7406bcaa4c2a3d304e92a9b478bed50e8044c2
AtefAnggaMalikFadilah <atefanggamf@gmail.com>
2025-01-22 10:04:09 +0700

    cleaning comment

--- index.js
+++ index.js
@@ -20,35 +20,4 @@ const router = require('./router')
 
 app.use('/api', router.karyawanRouter)
 
-// app.patch('/edit-karyawan/:id', (req, res) => {
-//     let dataUpdate = []
-//     for (let prop in req.body) {
-//         dataUpdate.push(`${prop} = ${db.escape(req.body[prop])}`)
-//     }
-
-//     let updateQuery = `UPDATE karyawan SET ${dataUpdate} WHERE id = ${req.params.id}`
-//     console.log(updateQuery)
-//     db.query(updateQuery, (err, results) => {
-//         if (err) res.status(500).send(err)
-
-//         db.query(`SELECT * FROM karyawan WHERE id = ${req.params.id}`, (err2, results2) => {
-//             if (err2) res.status(500).send(err2)
-//             res.status(200).send({ message: "Berhasil mengubah data", data: results2})
-//         })
-//     })
-// })
-
-// app.delete('/delete-karyawan/:id', (req, res) => {
-//     let deleteQuery = `DELETE FROM karyawan WHERE id = ${req.params.id}`
-//     let responseMessage = `{
-//             "status_code" : 200,
-//             "message" : "berhasil menghapus data"
-//         }`
-
-//     db.query(deleteQuery, (err, results) => {
-//         if (err) res.status(500).send(err)
-//         res.status(200).send(JSON.parse(responseMessage))
-//     })
-// })
-
 app.listen(PORT, () => console.log('listening on port', PORT))
